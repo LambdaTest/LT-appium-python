7dde1f74e1007c2bc5661fef450cb2efc0fc0a9c
likith-lt <likithk@lambdatest.com>
2022-06-14 14:31:29 +0530

    updated test scripts

--- ios.py
+++ ios.py
@@ -6,58 +6,73 @@ import time
 import os
 
 desired_caps = {
-    "deviceName":"iPhone 12",
-    "platformName":"ios",
-    "platformVersion":"14",
-    "isRealMobile":True,
-    "app":"lt://", #Enter app_url here
-    "build":"Python Vanilla iOS",
-    "name":"Sample Test - Python",
-    "network":True,
-    "visual":True,
-    "video":True
+    "deviceName": "iPhone 12",
+    "platformName": "ios",
+    "platformVersion": "14",
+    "isRealMobile": True,
+    "app": "APP_URL",  # Enter app_url here
+    "build": "Python Vanilla iOS",
+    "name": "Sample Test - Python",
+    "network": True,
+    "visual": True,
+    "video": True
 }
+
 
 def startingTest():
     if os.environ.get("LT_USERNAME") is None:
-        username = "username" #Enter LT username here if environment variables have not been added
+        # Enter LT username here if environment variables have not been added
+        username = "username"
     else:
         username = os.environ.get("LT_USERNAME")
     if os.environ.get("LT_ACCESS_KEY") is None:
-        accesskey = "accesskey" #Enter LT accesskey here if environment variables have not been added
+        # Enter LT accesskey here if environment variables have not been added
+        accesskey = "accesskey"
     else:
         accesskey = os.environ.get("LT_ACCESS_KEY")
 
     try:
-        driver = webdriver.Remote(desired_capabilities=desired_caps, command_executor="https://"+username+":"+accesskey+"@mobile-hub.lambdatest.com/wd/hub")
+        driver = webdriver.Remote(desired_capabilities=desired_caps, command_executor="https://" +
+                                  username+":"+accesskey+"@mobile-hub.lambdatest.com/wd/hub")
         time.sleep(3)
-        colorElement = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"color")))
+        colorElement = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "color")))
         colorElement.click()
-        textElement = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"Text")))
+        textElement = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "Text")))
         textElement.click()
-        toastElement = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"toast")))
+        toastElement = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "toast")))
         toastElement.click()
-        notification = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"notification")))
+        notification = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "notification")))
         notification.click()
         time.sleep(3)
-        geolocation = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"geoLocation")))
+        geolocation = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "geoLocation")))
         geolocation.click()
         time.sleep(5)
         driver.back()
-        home = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"Home")))
+        home = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "Home")))
         home.click()
-        speedTest = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"speedTest")))
+        speedTest = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "speedTest")))
         speedTest.click()
         time.sleep(5)
         driver.back()
-        browser = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"Browser")))
+        browser = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "Browser")))
         browser.click()
-        url = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID,"url")))
+        url = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "url")))
         url.send_keys("https://www.lambdatest.com")
-        find = WebDriverWait(driver,20).until(EC.element_to_be_clickable((MobileBsy.ACCESSIBILITY_ID,"find")))
+        find = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((MobileBsy.ACCESSIBILITY_ID, "find")))
         find.click()
         driver.quit()
     except:
         driver.quit()
+
 
 startingTest()
