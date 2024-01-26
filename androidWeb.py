95aff3a898542ae8c7d0fd61c27443750003ce07
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2024-01-26 15:14:48 +0530

    Update androidWeb.py

--- androidWeb.py
+++ androidWeb.py
@@ -1,4 +1,4 @@
-from appium import webdriver
+from selenium import webdriver
 from appium.webdriver.common.mobileby import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
